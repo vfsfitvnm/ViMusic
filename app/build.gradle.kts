ff3cf8f583b0dcbe15a11ab2977b219bfb5884dd
vfsfitvnm <vfsfitvnm@protonmail.com>
2022-06-18 17:35:45 +0200

    Bump version to 0.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "it.vfsfitvnm.vimusic"
         minSdk = 21
         targetSdk = 32
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.6"
     }
 
     splits {
