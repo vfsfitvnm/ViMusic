4e5f17de23dea8069d8446fd9d314752d4a448d5
vfsfitvnm <vfsfitvnm@protonmail.com>
2022-06-22 16:41:40 +0200

    Do not return songs that have never been played in Database.history()

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
@@ -47,7 +47,7 @@ interface Database {
     fun songWithInfo(id: String): SongWithInfo?
 
     @Transaction
-    @Query("SELECT * FROM Song ORDER BY ROWID DESC")
+    @Query("SELECT * FROM Song WHERE totalPlayTimeMs > 0 ORDER BY ROWID DESC")
     fun history(): Flow<List<SongWithInfo>>
 
     @Transaction
