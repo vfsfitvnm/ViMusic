06b4fcdc6337f2f8538d8d21cb44025043327b7e
vfsfitvnm <vfsfitvnm@protonmail.com>
2022-07-18 10:28:07 +0200

    Downgrade Gradle/AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build", "gradle", "7.4.0-alpha08")
+        classpath("com.android.tools.build", "gradle", "7.2.0")
         classpath(kotlin("gradle-plugin", libs.versions.kotlin.get()))
     }
 }
