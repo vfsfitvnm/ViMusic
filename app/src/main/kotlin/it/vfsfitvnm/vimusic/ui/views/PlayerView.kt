b978e9b006b5eda0feb2777fd39dc113857c927d
Malopieds <74783202+Malopieds@users.noreply.github.com>
2022-08-06 13:06:55 +0200

    Update PlayerView.kt
    FML

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/views/PlayerView.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/views/PlayerView.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
