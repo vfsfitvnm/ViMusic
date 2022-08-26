17a5e603b3bc587e5fc8816abc66018e46e7d298
vfsfitvnm <vfsfitvnm@protonmail.com>
2022-08-26 10:10:15 +0200

    Add enableComposeCompilerReports flag

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,17 @@ buildscript {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            if (project.findProperty("enableComposeCompilerReports") == "true") {
+                arrayOf("reports", "metrics").forEach {
+                    freeCompilerArgs = freeCompilerArgs + listOf(
+                        "-P", "plugin:androidx.compose.compiler.plugins.kotlin:${it}Destination=${project.buildDir.absolutePath}/compose_metrics"
+                    )
+                }
+            }
+        }
+    }
+}
