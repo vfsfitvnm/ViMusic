c72a55397e4c14268ea321d7923b71eb0a220ff2
pavlohural <pavlohural@gmail.com>
2022-10-07 14:25:37 +0300

    Format code

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/LocalPlaylistScreen.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/LocalPlaylistScreen.kt
@@ -224,29 +224,43 @@ fun LocalPlaylistScreen(playlistId: Long) {
                                                             transaction {
                                                                 runBlocking(Dispatchers.IO) {
                                                                     withContext(Dispatchers.IO) {
-                                                                        YouTube.playlist(browseId)?.map {
-                                                                            it.next()
-                                                                        }?.map { playlist ->
-                                                                            playlist.copy(items = playlist.items?.filter { it.info.endpoint != null })
-                                                                        }
-                                                                    }
-                                                                }?.getOrNull()?.let { remotePlaylist ->
-                                                                    Database.clearPlaylist(playlistWithSongs.playlist.id)
-
-                                                                    remotePlaylist.items?.forEachIndexed { index, song ->
-                                                                        song.toMediaItem(browseId, remotePlaylist)?.let { mediaItem ->
-                                                                            Database.insert(mediaItem)
-
-                                                                            Database.insert(
-                                                                                SongPlaylistMap(
-                                                                                    songId = mediaItem.mediaId,
-                                                                                    playlistId = playlistId,
-                                                                                    position = index
-                                                                                )
-                                                                            )
-                                                                        }
+                                                                        YouTube
+                                                                            .playlist(browseId)
+                                                                            ?.map {
+                                                                                it.next()
+                                                                            }
+                                                                            ?.map { playlist ->
+                                                                                playlist.copy(items = playlist.items?.filter { it.info.endpoint != null })
+                                                                            }
                                                                     }
                                                                 }
+                                                                    ?.getOrNull()
+                                                                    ?.let { remotePlaylist ->
+                                                                        Database.clearPlaylist(
+                                                                            playlistWithSongs.playlist.id
+                                                                        )
+
+                                                                        remotePlaylist.items?.forEachIndexed { index, song ->
+                                                                            song
+                                                                                .toMediaItem(
+                                                                                    browseId,
+                                                                                    remotePlaylist
+                                                                                )
+                                                                                ?.let { mediaItem ->
+                                                                                    Database.insert(
+                                                                                        mediaItem
+                                                                                    )
+
+                                                                                    Database.insert(
+                                                                                        SongPlaylistMap(
+                                                                                            songId = mediaItem.mediaId,
+                                                                                            playlistId = playlistId,
+                                                                                            position = index
+                                                                                        )
+                                                                                    )
+                                                                                }
+                                                                        }
+                                                                    }
                                                             }
                                                         }
                                                     )
