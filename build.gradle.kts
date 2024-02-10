2e693270cb84f0bf44a7807ee5fd8fe9b5112e8f
Sharun <sharunksplus@gmail.com>
2024-02-10 14:52:19 -0800

    upgrade build tools to 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build", "gradle", "7.3.0")
+        classpath("com.android.tools.build", "gradle", "7.4.2")
         classpath(kotlin("gradle-plugin", libs.versions.kotlin.get()))
     }
 }
